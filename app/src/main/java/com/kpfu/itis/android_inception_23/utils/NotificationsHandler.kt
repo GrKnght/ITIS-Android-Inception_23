d28ac77e12c8c292f4891a2571f2e57b369120b6
R.Faskhetdinov <rrfaskhetdinov@detmir.ru>
2023-11-14 16:51:14 +0300

    Notifications logic implemented

--- app/src/main/java/com/kpfu/itis/android_inception_23/utils/NotificationsHandler.kt
+++ app/src/main/java/com/kpfu/itis/android_inception_23/utils/NotificationsHandler.kt
@@ -2,9 +2,12 @@ package com.kpfu.itis.android_inception_23.utils
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.kpfu.itis.android_inception_23.MainActivity
 import com.kpfu.itis.android_inception_23.R
 
 class NotificationsHandler {
@@ -20,11 +23,21 @@ class NotificationsHandler {
                 manager.createNotificationChannel(channel)
             }
 
+            val intent = Intent(ctx, MainActivity::class.java)
+
+            val pendingIntent = PendingIntent.getActivity(
+                ctx,
+                101,
+                intent,
+                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+            )
 
             val notification = NotificationCompat.Builder(ctx, "first_notification_channel")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Notification title")
                 .setContentText("Message")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
 
             manager.notify(id, notification.build())
         }
